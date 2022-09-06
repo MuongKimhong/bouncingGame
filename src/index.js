c3d7877fd4b54fb47a515cc0615d6fb67e70f8ab
MuongKimhong <muongkimhong086@gmail.com>
2022-09-06 12:17:18 +1000

    Detect Game over

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-let ballRadius = 10;
-let dx = 2;
-let dy = -2;
-let paddleHeight = 10;
-let paddleWidth = 75;
-let paddleX = canvas.width / paddleWidth / 2;
-let paddleY = canvas.height - paddleHeight;
-let intervalTime = 10;
-let rightKeyPressed = false; // D key
-let leftKeyPressed = false; // A key
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var ballRadius = 10;
+var dx = 2;
+var dy = -2;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = canvas.width / paddleWidth / 2;
+var paddleY = canvas.height - paddleHeight;
+var intervalTime = 10;
+var rightKeyPressed = false; // D key
+var leftKeyPressed = false; // A key
+var gameOver = false;
+var runGameInterval;
 
 document.addEventListener("keydown", keyDownEventHandler, false);
 document.addEventListener("keyup", keyUpEventHandler, false);
@@ -50,12 +52,14 @@ function checkBallBouncing() {
   if (y + dy - ballRadius < 0 || checkBallTouchPaddle() == true) {
     dy = -dy;
   }
-  // y + dy + ballRadius > canvas.height - paddleHeight (bouncing bottom)
 
   // check bouncing right and left edge
   if (x + dx + ballRadius > canvas.width || x + dx - ballRadius < 0) {
     dx = -dx;
   }
+
+  // game over
+  if (y + dy + ballRadius > canvas.height) gameOver = true;
 }
 
 function drawBall() {
@@ -71,26 +75,31 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  drawPaddle();
-  let ball = drawBall(ctx, canvas.width, canvas.height, x, y);
+  if (gameOver == true) {
+    alert("Game over");
+    clearInterval(runGameInterval);
+  } else {
+    drawPaddle();
+    let ball = drawBall(ctx, canvas.width, canvas.height, x, y);
 
-  // check if paddle bounce right
-  if (rightKeyPressed == true) {
-    paddleX += 7;
-    if (paddleX + paddleWidth > canvas.width) {
-      paddleX = canvas.width - paddleWidth;
+    // check if paddle bounce right
+    if (rightKeyPressed == true) {
+      paddleX += 7;
+      if (paddleX + paddleWidth > canvas.width) {
+        paddleX = canvas.width - paddleWidth;
+      }
     }
-  }
-  // check if paddle bounce left
-  if (leftKeyPressed == true) {
-    paddleX -= 7;
-    if (paddleX < 0) {
-      paddleX = 0;
+    // check if paddle bounce left
+    if (leftKeyPressed == true) {
+      paddleX -= 7;
+      if (paddleX < 0) {
+        paddleX = 0;
+      }
     }
-  }
 
-  x += dx;
-  y += dy;
+    x += dx;
+    y += dy;
+  }
 }
 
-setInterval(draw, intervalTime);
+runGameInterval = setInterval(draw, intervalTime);
